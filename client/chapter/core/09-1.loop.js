68a58bc85c9caba2812f5b6bf6595646a827afaf
jiyoon <vywns9978@gmail.com>
2023-01-03 09:31:00 +0900

    [추가] 반복문 수업 코드 추가

--- client/chapter/core/09-1.loop.js
+++ client/chapter/core/09-1.loop.js
@@ -2,12 +2,13 @@
 /* While Loop                                                             */
 /* ---------------------------------------------------------------------- */
 
-// let i = 0;
-
-// while (i < 10) {
-//   console.log("안녕");
-//   i++;
-// }
+/* 
+let i = 0;
+while(i < 10){
+  console.log(++i);
+  
+}
+ */
 
 const frontEndDev = [
   "HTML",
@@ -28,22 +29,47 @@ const frontEndDev = [
 // console.log(frontEndDev[4]);
 // console.log(frontEndDev[5]);
 
+let i = 0;
+
+while (i < frontEndDev.length) {
+  let value = frontEndDev[i];
+
+  // console.log(i);
+
+  console.log(value);
+
+  i++;
+}
+
+console.log(i);
+
+let z1 = performance.now();
+let z2 = performance.now();
+
+console.log(z2 - z1);
+
+/* 
+let i = frontEndDev.length;
+while( i >= 0){
+  console.log(i, frontEndDev[i] );
+  i--
+}
+ */
+
+// 전개 연산자  전개 구문 spread oparator
+
+// let copyArray = [...frontEndDev];
+
+// let copyArray = frontEndDev.slice();
+
+// while(copyArray.length){
+//   console.log(copyArray.pop());
+// }
+
 /* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
 
 // while 문 (순환 : 순방향)
 
 // while 문 (역순환 : 역방향)
-
-// let i = frontEndDev.length - 1;
-
-// while (i > 0) {
-//   console.log(frontEndDev[i]);
-//   i--;
-// }
-
-while (frontEndDev.length) {
-  console.log(frontEndDev[frontEndDev.length - 1]);
-  frontEndDev.pop();
-}
 
 // 성능 진단 : 순환 vs. 역순환
